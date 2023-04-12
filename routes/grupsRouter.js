af1be3f2f7689e2b1c1060bdd8114066dbb7c30c
Eric García Castro <ericgar2003@gmail.com>
2023-04-12 17:40:42 +0200

    ACTUALIZACION ROUTER GRUPS

--- routes/grupsRouter.js
+++ routes/grupsRouter.js
@@ -1,21 +1,28 @@
 var express = require("express");
-var middelware = require("../middlewares/authenticate");
 var router = express.Router();
+var cors = require("cors");
+var authenticateToken = require("../middlewares/authenticateToken");
+
+const corsOptions = {
+    origin: "http://localhost:3000", // Cambia esto al dominio de tu frontend en producción
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+router.use(cors(corsOptions));
 
 const grup_controller = require("../controllers/grupsController");
 
-router.use([middelware.isAuth,middelware.hasRole('administrador')]);
+router.get("/", authenticateToken, grup_controller.list);
 
-router.get("/", grup_controller.list);
+router.get("/create", authenticateToken, grup_controller.create_get);
+router.post("/create", authenticateToken, grup_controller.create_post);
 
-router.get("/create", grup_controller.create_get);
-router.post("/create", grup_controller.rules,grup_controller.create_post);
+router.get("/delete/:id", authenticateToken, grup_controller.delete_get);
+router.post("/delete/:id", authenticateToken, grup_controller.delete_post);
 
-router.get("/delete/:id", grup_controller.delete_get);
-router.post("/delete/:id", grup_controller.delete_post);
-
-router.get("/update/:id", grup_controller.update_get);
-router.post("/update/:id", grup_controller.rules, grup_controller.update_post);
+router.get("/update/:id", authenticateToken, grup_controller.update_get);
+router.post("/update/:id", authenticateToken, grup_controller.update_post);
 
 
 module.exports = router;
